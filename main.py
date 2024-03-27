698ff382bf06fe2a615a50136f6f6c93eeecdd10
Kavinath p <Kavinkutty452@gmail.com>
2024-03-27 06:32:57 +0530

    final changes

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import pandas as pd
 import numpy as np
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 
-data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/dataset.csv", encoding='utf-8')
+data = pd.read_csv("./dataset.csv")
+# print(data.head())
 
-print(data.head().to_string())
-
-
-# let's look that dataset has null value or not..!
-# data.isnull.sum()
+# Let’s have a look at whether this dataset contains any null values or not
+data.isnull().sum()
 
 # let’s have a look at all the languages present in this dataset
-# data["languages"].values_count()
+data["language"].value_counts()
+
+#let’s split the data into training and test sets
+text = np.array(data["Text"])
+lang = np.array(data["language"])
+cv = CountVectorizer()
+X = cv.fit_transform(text)
+X_train, X_test, Y_train, Y_test = train_test_split(X, lang,test_size=0.33, random_state=42)
+
+# we  will be using the Multinomial Naïve Bayes algorithm to train the language detection model 
+model = MultinomialNB()
+model.fit(X_train,Y_train)
+model.score(X_test,Y_test)
+
+# let’s use this model to detect the language of a text by taking a user input
+user = input("Enter your Text: ")
+data = cv.transform([user]).toarray()
+output = model.predict(data)
+print(output)
